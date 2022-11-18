5a3a4ea38374778f2dfa29c298c5907f36606f2a
zen0no <dolgovyazovandrej@gmail.com>
2022-11-18 23:08:33 +0200

    Add terminal raw mode

--- src/botl.c
+++ src/botl.c
@@ -1,8 +1,52 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
+
+
+struct termios orig_termios;
+
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+void enableRawMode() {
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcsetattr");
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | IEXTEN | ICANON | ISIG);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+}
 
 int main()
 {
+    enableRawMode();
+
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (1){
+        c = '\0';
+
+        if (read(STDIN_FILENO, &c, 1) == -1) die("read");
+        if (iscntrl(c)){
+            printf("%d\r\n", c);
+        }
+        else
+        {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
     return 0;
 }
